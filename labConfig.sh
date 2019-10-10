0bb8806e7d86c2c263e44fa1d6788d74568c11eb
Yuxuan Wang <yw69@st-andrews.ac.uk>
2019-10-10 14:00:02 +0100

    10/10-2: Vuetify & lab machine configuration

---
+++ labConfig.sh
@@ -0,0 +1,5 @@
+# Usage: grant global package installation permission
+# Make sure latest node & npm installed on home directory
+# Run: . labConfig.sh
+
+export PATH=~/node-v12.11.1-linux-x64/bin:$PATH
