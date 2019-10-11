946c0b66ef63f4f7a52ed9498c1ebe175000c95b
Yuxuan Wang <yw69@st-andrews.ac.uk>
2019-10-11 13:59:55 +0100

    10/11-1: Start building navigation

---
+++ code/src/components/layout/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-navigation-drawer>
+        <v-list nav>
+            <div />
+
+            <v-list-item
+                v-for='(item, i) in items'
+                :key='i'
+            >
+                <v-list-item-title v-text='item.title' />
+
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: [
+            { title: '1st item' },
+            { title: '2nd item' }
+        ]
+    })
+}
+</script>>
