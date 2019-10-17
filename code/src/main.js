19796065a6ee489e22b750f656ceafb82e023532
Yuxuan Wang <yw69@st-andrews.ac.uk>
2019-10-17 14:58:37 +0100

    10/17-1: Add Konva package for graphics

--- code/src/main.js
+++ code/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router'
 import vuetify from './plugins/vuetify';
+import VueKonka from 'vue-konva';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueKonka);
 
 const paths = [
     { 
--- code/src/views/Dashboard.vue
+++ code/src/views/Dashboard.vue
@@ -2,12 +2,36 @@
     <v-container
     fluid
     >
-        <p>Dashboard should be here...</p>
+        <v-stage :config="konva_config" style="border: 1px solid black">
+            <v-layer>
+                <v-rect :config="rect" />
+                
+                <v-text :config="text" />
+            </v-layer>
+        </v-stage>
     </v-container>
 </template>
 
 <script>
 export default {
-    
+    data: () => ({
+        konva_config: {
+            width: window.innerWidth,
+            height: window.innerHeight
+        },
+        rect: {
+            x: 0,
+            y: 0,
+            width: 100,
+            height: 100,
+            fill: '#ffffff',
+            stroke: '#000000',
+            draggable: true
+        },
+        text: {
+            text: 'HelloWorld',
+            draggable: true
+        }
+    })
 }
 </script>
