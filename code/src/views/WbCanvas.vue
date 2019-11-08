91e0d4a6b8201d1cd54cca660caaf1aa4857d503
Yuxuan Wang <yw69@st-andrews.ac.uk>
2019-11-08 16:01:18 +0000

    11/8-1.1: Fix Konva canvas size bug

--- code/src/views/WbCanvas.vue
+++ code/src/views/WbCanvas.vue
@@ -11,10 +11,15 @@ export default {
     data() {
         return {
             konvaConfig: {
-                width: this.$parent.$refs.canvas.clientWidth - 8,
-                height: window.innerHeight - sessionStorage.getItem('topbarHeight') - 24
+                width: 0,
+                height: 0
             }
         }
+    },
+
+    mounted() {
+        this.konvaConfig.width = this.$parent.$refs.canvas.clientWidth - 8;
+        this.konvaConfig.height = window.innerHeight - sessionStorage.getItem('topbarHeight') - 24;
     }
 }
 </script>
